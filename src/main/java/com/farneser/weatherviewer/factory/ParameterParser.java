34e4dac6059dff71aef6bfd46b4b30a3cd1b6df5
farneser <farneser1@gmail.com>
2023-09-16 16:15:46 +0300

    feat: ParameterParser int, string

---
+++ src/main/java/com/farneser/weatherviewer/factory/ParameterParser.java
@@ -0,0 +1,23 @@
+package com.farneser.weatherviewer.factory;
+
+
+import jakarta.servlet.http.HttpServletRequest;
+
+public class ParameterParser {
+    public static String getStringParameter(HttpServletRequest request, String paramName) {
+        return request.getParameter(paramName);
+    }
+
+    public static int getIntParameter(HttpServletRequest request, String paramName) {
+        String paramValue = request.getParameter(paramName);
+        if (paramValue != null && !paramValue.isEmpty()) {
+            try {
+                return Integer.parseInt(paramValue);
+            } catch (NumberFormatException e) {
+                return 0;
+            }
+        }
+
+        return 0;
+    }
+}

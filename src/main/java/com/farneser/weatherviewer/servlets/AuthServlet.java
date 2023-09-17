66f6b2cefc0cf78517cc7b8c3f97e0cb1671425c
farneser <farneser1@gmail.com>
2023-09-17 03:24:31 +0300

    feat: auth servlet

---
+++ src/main/java/com/farneser/weatherviewer/servlets/AuthServlet.java
@@ -0,0 +1,27 @@
+package com.farneser.weatherviewer.servlets;
+
+import com.farneser.weatherviewer.dao.SessionDao;
+import com.farneser.weatherviewer.helpers.factory.HibernateFactory;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.util.Date;
+
+public class AuthServlet extends BaseServlet {
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        var sessionDao = new SessionDao(HibernateFactory.getSessionFactory().openSession());
+
+        var session = sessionDao.getById(getSessionId(req));
+
+        if (session != null && session.getExpiresAt().getTime() > new Date().getTime()) {
+            super.service(req, resp);
+        }
+
+        templateEngine.process("login", context, resp.getWriter());
+    }
+}

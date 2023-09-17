afb4a97430158189460a28e6b238bd78a13ecac8
farneser <farneser1@gmail.com>
2023-09-17 03:14:09 +0300

    fix: login servlet

---
+++ src/main/java/com/farneser/weatherviewer/servlets/LoginServlet.java
@@ -0,0 +1,59 @@
+package com.farneser.weatherviewer.servlets;
+
+import com.farneser.weatherviewer.dao.SessionDao;
+import com.farneser.weatherviewer.dao.UserDao;
+import com.farneser.weatherviewer.helpers.factory.HibernateFactory;
+import com.farneser.weatherviewer.helpers.factory.UserDtoFactory;
+import com.farneser.weatherviewer.helpers.utils.PasswordUtil;
+import com.farneser.weatherviewer.helpers.utils.SessionUtil;
+import com.farneser.weatherviewer.models.User;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(name = "Login page", urlPatterns = "/login")
+public class LoginServlet extends BaseServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws IOException {
+        templateEngine.process("login", context, response.getWriter());
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws IOException {
+        var loginDto = UserDtoFactory.getLogin(request);
+
+        var userDao = new UserDao(HibernateFactory.getSessionFactory().openSession());
+        var sessionDao = new SessionDao(HibernateFactory.getSessionFactory().openSession());
+
+        User user = null;
+
+        if (loginDto.getUsername() != null) {
+            user = userDao.getByUsername(loginDto.getUsername());
+        }
+
+        if (user == null || !PasswordUtil.isPasswordCorrect(loginDto.getPassword(), user.getPassword())) {
+
+            context.setVariable("errorMessage", "The username or password you entered is incorrect");
+            templateEngine.process("login", context, response.getWriter());
+
+            return;
+        }
+
+        var session = SessionUtil.build(user);
+
+        sessionDao.create(session);
+
+        var cookie = new Cookie("sessionId", session.getId().toString());
+
+        cookie.setMaxAge((int) ((session.getExpiresAt().getTime() - System.currentTimeMillis()) / 1000));
+
+        response.addCookie(cookie);
+
+        response.sendRedirect("/dashboard");
+    }
+}

81e9988940f3022a0019bbbf8e51a89649db3976
farneser <farneser1@gmail.com>
2023-09-17 04:48:57 +0300

    feat: root page servlet

---
+++ src/main/java/com/farneser/weatherviewer/servlets/RootServlet.java
@@ -0,0 +1,15 @@
+package com.farneser.weatherviewer.servlets;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "Root page", urlPatterns = "/")
+public class RootServlet extends AuthServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) {
+
+        System.out.println("root page");
+    }
+
+}
